2dd51bd302b7f3493706bfcd4dd74e82337ea044
Bryan Cady <sw@listentothewindmedia.com>
2017-09-13 14:35:12 -0400

    Viewport mod

--- js/backstretch-set.js
+++ js/backstretch-set.js
@@ -1,13 +1,23 @@
-/**
- * This script sets the Backstretch arguments in the Digital Pro Theme.
- *
- * @package Digital\JS
- * @author StudioPress
- * @license GPL-2.0+
- */
+;(function ( $, window, document, undefined ) {
+	'use strict'
 
-jQuery(document).ready(function($) {
+	function initBackstretch() {
+		var images = $.parseJSON( BackStretchImg2.src );
 
-	$(".front-page-1").backstretch([BackStretchImg.src]);
+		$(".front-page-1").backstretch( images,{
+			duration: 5000,
+			fade: 750
+		});
+	}
 
-});
+	$( document ).ready( function () {
+		if ( typeof BackStretchImg2 === 'object' ) {
+			initBackstretch();
+		}
+
+		if( BackStretchImg1.src != '' ) {
+			$(".front-page-1").backstretch([BackStretchImg1.src]);
+		}
+	} );
+
+}( jQuery, window, document ));
